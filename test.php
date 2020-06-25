bac91e03e861fbf7317e697da3b9b43fc08929f2
Gregor <gregor-j@users.noreply.github.com>
2020-06-25 11:35:01 +0200

    update test script to cover new getFunctionDescription()

--- test.php
+++ test.php
@@ -25,8 +25,49 @@ if (!is_array($config)) {
         $configFile
     ));
 }
-echo 'Calling RFC_PING ... ';
+/**
+ * Create connection instance to SAP using the config.
+ */
 $connection = new \SAPNWRFC\Connection($config);
-$function = $connection->getFunction('RFC_PING');
-$result = $function->invoke();
+/**
+ * First test: Call RFC_PING to see if remote function calls work.
+ */
+echo 'Calling RFC_PING ... ';
+try {
+    $rfcPing = $connection->getFunction('RFC_PING');
+    $result = $rfcPing->invoke();
+} catch (Exception $exception) {
+    echo 'FAIL! ' . get_class($exception) . ': ' . $exception->getMessage() . PHP_EOL;
+    exit(1);
+}
+echo 'OK.' . PHP_EOL;
+/**
+ * Second test step 1: Get the description of a remote function.
+ */
+echo 'Get description of RFC_WALK_THRU_TEST ... ';
+$walkthrough = $connection->getFunction('RFC_WALK_THRU_TEST');
+if (!method_exists($walkthrough, 'getFunctionDescription')) {
+    echo 'FAIL! ' . get_class($walkthrough) . '::getFunctionDescription() doesn\'t exist.' . PHP_EOL;
+    exit(1);
+}
+/**
+ * Second test step 2: Rudimentary check whether the description is as expected.
+ */
+$description = $walkthrough->getFunctionDescription();
+if (!is_array($description) || !array_key_exists('TEST_OUT', $description)) {
+    echo 'FAIL! Cannout find TEST_OUT in description.' . PHP_EOL;
+    exit(1);
+}
+if (!is_array($description['TEST_OUT'])
+    || !array_key_exists('typedef', $description['TEST_OUT'])
+) {
+    echo 'FAIL! Cannout find TEST_OUT.typedef in description.' . PHP_EOL;
+    exit(1);
+}
+if (!is_array($description['TEST_OUT']['typedef'])
+    || !array_key_exists('RFCCHAR1', $description['TEST_OUT']['typedef'])
+) {
+    echo 'FAIL! Cannout find TEST_OUT.typedef.RFCCHAR1 in description.' . PHP_EOL;
+    exit(1);
+}
 echo 'OK.' . PHP_EOL;
